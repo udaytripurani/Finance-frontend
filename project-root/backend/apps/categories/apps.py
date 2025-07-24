8d3e0b7ec5865e01e8ed07c90bddd6a436801b19
Gautam Chaurasiya <150584596+1-SNK-KOS@users.noreply.github.com>
2025-07-24 18:50:55 +0530

    Delete project-root directory

--- project-root/backend/apps/categories/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CategoriesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.categories'
-    label = 'categories'
--- project-root/backend/apps/transactions/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Income, Expense
-
-admin.site.register(Income)
-admin.site.register(Expense)
